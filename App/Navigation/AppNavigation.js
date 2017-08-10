fd9dc17dba526a7729e395ff73cab9f4baf042a0
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2017-08-10 08:40:28 +0500

    registration was added to navigation

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,18 +1,21 @@
 import { StackNavigator } from 'react-navigation'
 import LaunchScreen from '../Containers/LaunchScreen'
+import RegisterScreen from '../Containers/RegisterScreen'
 
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = StackNavigator({
-  LaunchScreen: { screen: LaunchScreen }
+  LaunchScreen: { screen: LaunchScreen },
+  RegisterScreen: { screen: RegisterScreen } 
+  
 }, {
   // Default config for all screens
   headerMode: 'none',
   initialRouteName: 'LaunchScreen',
   navigationOptions: {
     headerStyle: styles.header
-  }
+  },  
 })
 
 export default PrimaryNav
