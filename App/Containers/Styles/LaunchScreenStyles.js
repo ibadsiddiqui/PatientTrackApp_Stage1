3587d0b88f8435aee1c08166ae1fa0d299c092fb
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2017-08-10 08:37:44 +0500

    styling to the launch screen was added

--- App/Containers/Styles/LaunchScreenStyles.js
+++ App/Containers/Styles/LaunchScreenStyles.js
@@ -1,18 +1,62 @@
 import { StyleSheet } from 'react-native'
 import { Metrics, ApplicationStyles } from '../../Themes/'
-
 export default StyleSheet.create({
   ...ApplicationStyles.screen,
   container: {
-    paddingBottom: Metrics.baseMargin
+    // paddingBottom: Metrics.baseMargin
+    backgroundColor: "#fff"
+
   },
   logo: {
-    marginTop: Metrics.doubleSection,
-    height: Metrics.images.logo,
-    width: Metrics.images.logo,
-    resizeMode: 'contain'
+    marginTop: 80,
+    marginLeft: 50,
+    color: "#E64A19",
+    fontWeight: "700",
+    fontSize: 30,
   },
   centered: {
     alignItems: 'center'
-  }
-})
+  },
+  inputEmail:{
+    color: "#E64A19",
+  },  
+  inputPass:{
+    color: "#E64A19",
+  },
+  loginSection:{
+    margin: 0,
+    marginTop: 100
+  },
+  submitBtn1:{
+    alignItems: 'center',    
+    justifyContent: 'center', 
+    backgroundColor: 'transparent',
+    borderWidth: 1,
+    borderColor: 'transparent',
+    borderRadius: 5,
+    marginBottom: 20,
+    marginLeft: 1,
+    paddingLeft: 20,
+    paddingRight: 20,
+    marginTop: 50,
+    width: 350,
+    height: 40,
+    },
+    submitBtn2:{
+      alignItems: 'center',
+      backgroundColor: 'transparent',
+      borderWidth: 1,
+      borderColor: 'transparent',
+      borderRadius: 5,
+      height: 40,
+      justifyContent: 'center',
+      marginTop: 150,
+      marginLeft: 5,
+      
+      width: 350,
+    },
+    submitText:{
+        color: '#E64A19',
+        fontWeight: '600'
+    },
+});
