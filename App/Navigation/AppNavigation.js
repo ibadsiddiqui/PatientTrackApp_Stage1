263ec8e6fdf8c033f0da9edacaeb18da7f792b63
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2017-08-10 08:50:35 +0500

    dektopScreen navigation was added

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,13 +1,15 @@
 import { StackNavigator } from 'react-navigation'
 import LaunchScreen from '../Containers/LaunchScreen'
 import RegisterScreen from '../Containers/RegisterScreen'
+import DesktopScreen from '../Containers/DesktopScreen'
 
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = StackNavigator({
   LaunchScreen: { screen: LaunchScreen },
-  RegisterScreen: { screen: RegisterScreen } 
+  RegisterScreen: { screen: RegisterScreen },
+  DesktopScreen: {screen: DesktopScreen} 
   
 }, {
   // Default config for all screens
